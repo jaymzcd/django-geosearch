5ed35cb24157284d33c37647e0a5799259dfca0b
jaymz <jaymz@mail.u-dox.com>
2010-08-23 15:21:26 +0100

    add a distance to latlong method, change return to zipcode + distance from point

--- models.py
+++ models.py
@@ -18,6 +18,26 @@ class ZipCodeEntry(models.Model):
     def __unicode__(self):
         return self.zipcode
 
+    def distance_to_latlong(self, latlong):
+        """ Returns the distance (in miles) of a zipcode's lat/long to
+        a given target lat/long. This is basically a conversion of the
+        javascript here: http://www.movable-type.co.uk/scripts/latlong.html """
+
+        EARTH_RADIUS = 3440.07 # in n.miles
+        NAUTICAL_TO_MILE_CONV = 1.15077945 # convert n.miles back to miles
+
+        d_lat =  ZipCodeEntry.degrees_to_radians(latlong[0] - self.latitude)
+        d_long =  ZipCodeEntry.degrees_to_radians(latlong[1] - self.longitude)
+        source_lat = ZipCodeEntry.degrees_to_radians(latlong[0])
+        target_lat = ZipCodeEntry.degrees_to_radians(self.latitude)
+
+        hv_a = math.sin(d_lat/2)*math.sin(d_lat/2) + math.cos(source_lat)*math.cos(target_lat)*\
+            math.sin(d_long/2)*math.sin(d_long/2)
+        hv_c = 2*math.atan2(math.sqrt(hv_a), math.sqrt(1-hv_a))
+        hv_distance = EARTH_RADIUS * hv_c
+
+        return hv_distance * NAUTICAL_TO_MILE_CONV
+
     @staticmethod
     def degrees_to_radians(value):
         """ Returns a Lat/Long degree value to radian equivilant """
@@ -37,7 +57,7 @@ class ZipCodeEntry(models.Model):
 
     @staticmethod
     def calculate_latitude(source_lat, distance, heading):
-        """ Calculates the latitide for a source point given a certain
+        """ Calculates the latitude for a source point given a certain
         distance and heading direction. Values are in radians """
         return math.asin(math.sin(source_lat)*math.cos(distance) + \
             math.cos(source_lat)*math.sin(distance)*math.cos(heading))
@@ -68,7 +88,7 @@ class ZipCodeEntry(models.Model):
 
         The actual query filters based on a box around the point, not
         a circle. So the end result is the area is around 22% larger
-        than the "true" size. This can be further filtered on the client
+        than the "true" size. This can be further filtered on thesclient
         side or in the views rather than creating a complicated
         query. For most searches it's probably not a major problem
         in any case. """
@@ -99,5 +119,11 @@ class ZipCodeEntry(models.Model):
             latitude__gte=str(boundries[2][0]), longitude__gte=str(boundries[1][1]),
             longitude__lte=str(boundries[3][1]))
 
-        return zipcodes
+        code_data = list()
+        for code in zipcodes:
+            code_data.append(dict(zipcode=code.zipcode, \
+                distance=code.distance_to_latlong((zipcode.latitude, zipcode.longitude)))
+            )
+
+        return code_data
 
