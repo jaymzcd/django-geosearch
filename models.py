20ac50092812df38bb5cbf911429802a56d0bc77
jaymz <jaymz@u-dox.com>
2010-10-27 12:11:29 +0000

    Converting to a generic type, use zipsearch branch for original

--- models.py
+++ models.py
@@ -1,19 +1,25 @@
 from django.db import models
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 import math
 
-class ZipCodeEntry(models.Model):
-    """ Holds zipcode location data. This is based off the col's in
+class GeoEntry(models.Model):
+    """ Holds location data. This is based off the col's in
     the file via: http://www.populardata.com/zipcode_database.html
     The forumlas are based off nautical mile distances converted to
     radians so we need to convert *miles* to these n.miles. """
 
-    zipcode = models.CharField(max_length=5)
     latitude = models.DecimalField(decimal_places=6, max_digits=9)
     longitude = models.DecimalField(decimal_places=6, max_digits=9)
     city_name = models.CharField(max_length=25)
     state = models.CharField(max_length=2)
     county = models.CharField(max_length=25)
     format = models.CharField(max_length=50)
+
+    # Our generic binding
+    content_type = models.ForeignKey(ContentType, blank=True, null=True) 
+    object_id = models.PositiveIntegerField(blank=True, null=True)
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
 
     def __unicode__(self):
         return self.zipcode
@@ -26,10 +32,10 @@ class ZipCodeEntry(models.Model):
         EARTH_RADIUS = 3440.07 # in n.miles
         NAUTICAL_TO_MILE_CONV = 1.15077945 # convert n.miles back to miles
 
-        d_lat =  ZipCodeEntry.degrees_to_radians(latlong[0] - self.latitude)
-        d_long =  ZipCodeEntry.degrees_to_radians(latlong[1] - self.longitude)
-        source_lat = ZipCodeEntry.degrees_to_radians(latlong[0])
-        target_lat = ZipCodeEntry.degrees_to_radians(self.latitude)
+        d_lat =  GeoEntry.degrees_to_radians(latlong[0] - self.latitude)
+        d_long =  GeoEntry.degrees_to_radians(latlong[1] - self.longitude)
+        source_lat = GeoEntry.degrees_to_radians(latlong[0])
+        target_lat = GeoEntry.degrees_to_radians(self.latitude)
 
         hv_a = math.sin(d_lat/2)*math.sin(d_lat/2) + math.cos(source_lat)*math.cos(target_lat)*\
             math.sin(d_long/2)*math.sin(d_long/2)
@@ -109,25 +115,25 @@ class ZipCodeEntry(models.Model):
         HEADINGS = enumerate([0, math.pi/2, math.pi, 3*math.pi/2])
 
         try:
-            zipcode = ZipCodeEntry.objects.get(zipcode=_zipcode)
-        except ZipCodeEntry.DoesNotExist:
+            zipcode = GeoEntry.objects.get(zipcode=_zipcode)
+        except GeoEntry.DoesNotExist:
             # Maybe handle more gracefully?
             raise Exception('No zipcode matching query')
 
-        source_lat = ZipCodeEntry.degrees_to_radians(zipcode.latitude)
-        source_long = ZipCodeEntry.degrees_to_radians(zipcode.longitude)
-        distance = ZipCodeEntry.miles_to_radians(radius)
+        source_lat = GeoEntry.degrees_to_radians(zipcode.latitude)
+        source_long = GeoEntry.degrees_to_radians(zipcode.longitude)
+        distance = GeoEntry.miles_to_radians(radius)
 
         boundries = []
 
         for (cnt, heading) in HEADINGS:
-            target_lat = ZipCodeEntry.calculate_latitude(source_lat, distance, heading)
-            target_long = ZipCodeEntry.calculate_longitude(target_lat, source_long, distance, heading)
+            target_lat = GeoEntry.calculate_latitude(source_lat, distance, heading)
+            target_long = GeoEntry.calculate_longitude(target_lat, source_long, distance, heading)
 
-            boundries.append([ZipCodeEntry.radians_to_degrees(target_lat), \
-                ZipCodeEntry.radians_to_degrees(target_long)])
+            boundries.append([GeoEntry.radians_to_degrees(target_lat), \
+                GeoEntry.radians_to_degrees(target_long)])
 
-        zipcodes = ZipCodeEntry.objects.all().filter(latitude__lte=str(boundries[0][0]),
+        zipcodes = GeoEntry.objects.all().filter(latitude__lte=str(boundries[0][0]),
             latitude__gte=str(boundries[2][0]), longitude__gte=str(boundries[1][1]),
             longitude__lte=str(boundries[3][1]))
 
